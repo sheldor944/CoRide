0a911912ca44ca3be9771d2d371bdf02378735f0
tahsinchoudhury <tahsinbd247@gmail.com>
2023-10-23 19:56:34 +0600

    updated view profile file name

--- app/src/main/java/com/example/myapplication/ui/notifications/ViewProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/ViewProfileFragment.java
@@ -1,35 +1,21 @@
 package com.example.myapplication.ui.notifications;
 
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import android.Manifest;
-
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.MyFirebaseMessagingService;
 import com.example.myapplication.R;
-import com.example.myapplication.UserModel;
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
-import com.example.myapplication.ui.login.LoginActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
@@ -38,9 +24,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.io.IOException;
-
-public class NotificationsFragment extends Fragment {
+public class ViewProfileFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
     private static final String TAG = "NotificationsFragment";
