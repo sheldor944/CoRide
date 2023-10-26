0f6766b12705d1c4c167428daa6c237d7c0aef0f
sheldor944 <mehrajul.abedin.944@gmail.com>
2023-10-26 17:38:40 +0600

    RegisterActivity updated

--- app/src/main/java/com/example/myapplication/UserModel.java
+++ app/src/main/java/com/example/myapplication/UserModel.java
@@ -7,10 +7,20 @@ public class UserModel {
     private String phone;
     private String NationalID;
     private String password;
+    private String imageURL;
 
 
     public UserModel(){}
-    public UserModel(String firstName, String lastName, String email, String phone, String NationalID , String password) {
+    public UserModel(String firstName, String lastName, String email, String phone, String NationalID , String password , String imageURL) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.NationalID = NationalID;
+        this.password = password;
+        this.imageURL = imageURL;
+    }
+    public UserModel(String firstName, String lastName, String email, String phone, String NationalID , String password ) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -22,7 +32,7 @@ public class UserModel {
     public String getFirstName() {
         return firstName;
     }
-
+    public String getImageURL(){return  imageURL;}
     public String getLastName() {
         return lastName;
     }
