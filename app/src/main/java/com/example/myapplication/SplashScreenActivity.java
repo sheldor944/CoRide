75c5dfda5a08a938946d602ae0300b2715d7e3af
Tajri <tajri6547@gmail.com>
2023-10-29 00:43:27 +0600

    added splash screen

--- app/src/main/java/com/example/myapplication/SplashScreenActivity.java
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.java
@@ -1,13 +1,26 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import pl.droidsonroids.gif.GifImageView;
 
 public class SplashScreenActivity extends AppCompatActivity {
+    private GifImageView gifImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        gifImageView = findViewById(R.id.gifImageView);
+
+        new Handler().postDelayed(() -> {
+            Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+            startActivity(mainIntent);
+            finish();
+        }, 3490);
     }
 }
